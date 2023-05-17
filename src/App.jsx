e2d5f31e4a9d9a6cf196aecfaa508391370b8838
JohanCodeForFun <hellberg.johan@hotmail.com>
2023-05-17 10:07:43 +0200

    setup qoute api to fetch data and return it

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,41 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
 
 function App() {
+
+  // Quote Garden - A REST API for quotes.
+  const API_ENDPOINT = "https://quote-garden.onrender.com/api/v3/quotes";
+
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    fetch(API_ENDPOINT)
+    .then(response => {
+      if (!response.ok) {
+        throw new Error(
+          `This is an HTTP error: The status is ${response.status}`
+        )
+      }
+      return response.json()
+    })
+    .then(result => {
+      setData(result.data);
+      setError(null)
+    })
+    .catch(err => {
+      setError(err)
+      setData(null)
+      console.error(err)
+    })
+    .finally(() => {
+      setLoading(false)
+    })
+  }, []);
+  
+  console.log(data)
+
 
   return (
     <>
