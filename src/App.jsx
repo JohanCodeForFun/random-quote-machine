d3889c165a367166b3a6d117729bdeb594d6ce2c
JohanCodeForFun <hellberg.johan@hotmail.com>
2023-05-19 21:37:33 +0200

    extract content to components

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,7 @@
-import { useEffect, useState } from 'react'
-import './App.css'
+import { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
-
   // Quote Garden - A REST API for quotes.
   const API_ENDPOINT = "https://quote-garden.onrender.com/api/v3/quotes";
 
@@ -12,50 +11,66 @@ function App() {
 
   useEffect(() => {
     fetch(API_ENDPOINT)
-    .then(response => {
-      if (!response.ok) {
-        throw new Error(
-          `This is an HTTP error: The status is ${response.status}`
-        )
-      }
-      return response.json()
-    })
-    .then(result => {
-      setData(result.data);
-      setError(null)
-    })
-    .catch(err => {
-      setError(err)
-      setData(null)
-      console.error(err)
-    })
-    .finally(() => {
-      setLoading(false)
-    })
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error(
+            `This is an HTTP error: The status is ${response.status}`
+          );
+        }
+        return response.json();
+      })
+      .then((result) => {
+        setData(result.data);
+        setError(null);
+      })
+      .catch((err) => {
+        setError(err);
+        setData(null);
+        console.error(err);
+      })
+      .finally(() => {
+        setLoading(false);
+      });
   }, []);
-  
-  console.log(data)
 
+  console.log(data);
+
+  const QuoteComponent = () => (
+    <>
+      <div id="text">
+        <h3>
+          &quot; Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni
+          molestias quas obcaecati deleniti? &quot;
+        </h3>
+      </div>
+      <div id="author">
+        <p>- author</p>
+      </div>
+    </>
+  );
+
+  const ButtonComponent = () => (
+      <div id="button-box">
+        <button id="tweet-quote">tweet</button>
+        <button id="new-quote">new qoute</button>
+      </div>
+  );
+
+  const FooterComponent = () => (
+    <div id="created-box">
+      <p>by Johan</p>
+    </div>
+  )
 
   return (
     <>
-      <div id='quote-box'>
-        <div id='text'>
-          <h3>&quot; Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni molestias quas obcaecati deleniti? &quot;</h3>
-        </div>
-        <div id='author'>
-          <p>- author</p>
-        </div>
-        <div id='button-box'>
-          <button id='tweet-quote'>tweet</button>
-          <button id='new-quote'>new qoute</button>
-        </div>
-       </div>
-       <div id='created-box'>
-        <p>by Johan</p>
-       </div>
+      <div id="quote-box">
+        <QuoteComponent />
+        <ButtonComponent />
+      </div>
+      <FooterComponent />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
