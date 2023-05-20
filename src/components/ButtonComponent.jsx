5f226714b86cd8e4036904e5813f9114cf7c9003
JohanCodeForFun <hellberg.johan@hotmail.com>
2023-05-20 20:41:34 +0200

    move buttons to qoute component

--- src/components/ButtonComponent.jsx
+++
@@ -1,8 +0,0 @@
-const ButtonComponent = () => (
-  <div id="button-box">
-    <button id="tweet-quote">tweet</button>
-    <button id="new-quote">new qoute</button>
-  </div>
-);
-
-export default ButtonComponent;
