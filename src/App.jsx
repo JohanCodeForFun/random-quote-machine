28e225ea24c938a178e358412304b92aca720289
JohanCodeForFun <hellberg.johan@hotmail.com>
2023-05-20 17:27:28 +0200

    move qoute fetch to child component

--- src/App.jsx
+++ src/App.jsx
@@ -5,43 +5,11 @@ import ButtonComponent from "./components/ButtonComponent";
 import FooterComponent from "./components/FooterComponent";
 
 function App() {
-  // Quote Garden - A REST API for quotes.
-  const API_ENDPOINT = "https://quote-garden.onrender.com/api/v3/quotes";
-
-  const [data, setData] = useState(null);
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState(null);
-
-  useEffect(() => {
-    fetch(API_ENDPOINT)
-      .then((response) => {
-        if (!response.ok) {
-          throw new Error(
-            `This is an HTTP error: The status is ${response.status}`
-          );
-        }
-        return response.json();
-      })
-      .then((result) => {
-        setData(result.data);
-        setError(null);
-      })
-      .catch((err) => {
-        setError(err);
-        setData(null);
-        console.error(err);
-      })
-      .finally(() => {
-        setLoading(false);
-      });
-  }, []);
-
-  console.log(data);
 
   return (
     <>
       <div id="quote-box">
-        <QuoteComponent author="Johan" text="Hellooo"/>
+        <QuoteComponent  author="Johan" text="Hellooo" />
         <ButtonComponent />
       </div>
       <FooterComponent />
