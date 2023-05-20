e3b3050ebf11192ad2fce9a06e6e229a0ccab8bc
JohanCodeForFun <hellberg.johan@hotmail.com>
2023-05-20 21:27:08 +0200

    add link to portfolio

--- src/components/FooterComponent.jsx
+++ src/components/FooterComponent.jsx
@@ -1,6 +1,6 @@
 const FooterComponent = () => (
   <div id="created-box">
-    <p>by Johan</p>
+    <p>by Johan, <a href="https://jhellberg.com/" target="_blank" rel="noreferrer">Link to portfolio.</a> &copy; 2023</p>
   </div>
 )
 
